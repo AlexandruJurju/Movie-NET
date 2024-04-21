02ec02bdb74ec56a9d52f67ba862e9deddb466eb
Alex <alexandru.i.jurju@gmail.com>
2024-04-21 08:57:21 +0300

    upload movie without poster image

--- Movie-Net-Frontend/src/app/pages/upload-movie/upload-movie.component.ts
+++ Movie-Net-Frontend/src/app/pages/upload-movie/upload-movie.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
+import {MovieService} from "../../service/movie.service";
+import {Movie} from "../../model/movie";
 
 @Component({
   selector: 'app-upload-movie',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './upload-movie.component.html',
   styleUrl: './upload-movie.component.css'
 })
 export class UploadMovieComponent {
 
+  movieService: MovieService = inject(MovieService);
+
+  onSubmit(movieForm: NgForm) {
+    const movie: Movie = movieForm.value;
+
+    this.movieService.saveMovie(movie).subscribe({
+      next: (response) => {
+        console.log('Upload successful', response);
+      },
+      error: (error) => {
+        console.error('Upload failed', error);
+      }
+    });
+
+  }
 }
