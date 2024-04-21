86dbbab56834c641df142a883a46640b7fda3f46
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2024-04-21 08:42:34 +0300

    find all movies page

--- Movie-Net-Frontend/src/app/pages/get-movies/get-movies.component.ts
+++ Movie-Net-Frontend/src/app/pages/get-movies/get-movies.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Movie} from "../../model/movie";
+import {MovieService} from "../../service/movie.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-get-movies',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './get-movies.component.html',
   styleUrl: './get-movies.component.css'
 })
-export class GetMoviesComponent {
 
+export class GetMoviesComponent {
+  movies: Movie[] = [];
+  movieService: MovieService = inject(MovieService);
+
+  getMovies() {
+    this.movieService.getAllMovies()
+      .subscribe(movies => {
+        this.movies = movies;
+      })
+  }
 }
