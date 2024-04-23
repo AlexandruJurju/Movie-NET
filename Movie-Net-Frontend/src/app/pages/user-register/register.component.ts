86f82c99993707a45a3e9959a0fe730f263611a9
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2024-04-23 15:12:15 +0300

    implemented register component

--- Movie-Net-Frontend/src/app/pages/user-register/register.component.ts
+++ Movie-Net-Frontend/src/app/pages/user-register/register.component.ts
@@ -1,26 +1,62 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component} from '@angular/core';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services";
+import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  fb = inject(FormBuilder);
+
+  errorMessage: string | null = null;
 
   // validate form data
-  form = this.fb.nonNullable.group({
-    username: ['', Validators.required],
-    email: ['', Validators.required],
-    password: ['', Validators.required]
+  form = this.formBuilder.nonNullable.group({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
 
-  onSubmit() {
-
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private formBuilder: FormBuilder) {
   }
+
+  onSubmit() {
+    console.log("register")
+    if (this.form.valid) {
+      const registerRequestDto = {
+        username: this.form.value.username!,
+        email: this.form.value.email!,
+        password: this.form.value.password!,
+      }
+      this.register(registerRequestDto);
+    }
+  }
+
+  private register(registerRequestDto: { username: string, email: string, password: string }) {
+    this.authenticationService.registerUser(registerRequestDto).subscribe({
+      next: (response) => {
+        console.log("Registration successful", response);
+
+        // navigate to login page
+        this.router.navigate(["/user-login"]).then(() => {
+        })
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+        this.errorMessage = error.error.reasons[0].message;
+      }
+    })
+  }
+
 }
