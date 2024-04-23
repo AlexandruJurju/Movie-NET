e536f324c657d95a2c3fc8d656f4d66bbe6bc221
Alex <alexandru.i.jurju@gmail.com>
2024-04-23 14:57:04 +0300

    implemented login

--- Movie-Net-Frontend/src/app/pages/user-login/login.component.ts
+++ Movie-Net-Frontend/src/app/pages/user-login/login.component.ts
@@ -1,5 +1,7 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService, LoginRequestDto} from "../../services";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-login',
@@ -11,14 +13,46 @@ import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  fb = inject(FormBuilder);
 
-  form = this.fb.nonNullable.group({
-    username: ['', Validators.required],
-    password: ['', Validators.required]
-  });
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private formBuilder: FormBuilder) {
+  }
+
+  form = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(1)]],
+  })
 
   onSubmit() {
-
+    console.log("login")
+    if (this.form.valid) {
+      const loginRequestDto: LoginRequestDto = {
+        email: this.form.value.email!,
+        password: this.form.value.password!,
+      }
+      this.login(loginRequestDto);
+    }
   }
+
+
+  private login(loginRequestDto: LoginRequestDto) {
+    this.authenticationService.loginUser(loginRequestDto).subscribe({
+      next: (response) => {
+        console.log("Login successful logged in", response);
+
+        // save token to local storage
+        localStorage.setItem('token', response.token);
+        console.log(response.token);
+
+        this.router.navigate(["/home"]).then(() => {
+        })
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      }
+    })
+  }
+
 }
