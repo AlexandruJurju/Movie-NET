e481c9f98f8b88bce8918973a049be0f98b78fc4
Alex <alexandru.i.jurju@gmail.com>
2024-05-23 14:47:05 +0300

    post reviews

--- Movie-Net-Frontend/src/app/pages/movie-details/movie-details.component.ts
+++ Movie-Net-Frontend/src/app/pages/movie-details/movie-details.component.ts
@@ -1,36 +1,40 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
-import {DetailedMovieDto, MovieService} from "../../services/swagger";
+import {DetailedMovieDto, MovieService, ReviewDto, ReviewService} from "../../services/swagger";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
   imports: [
-    RouterLink,
     NgForOf,
-    NgIf
+    NgIf,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+  styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
   movie: DetailedMovieDto = {} as DetailedMovieDto;
+  form: FormGroup;
 
   constructor(
     private movieService: MovieService,
+    private reviewService: ReviewService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {
+    this.form = this.formBuilder.group({
+      text: ['', [Validators.required]],
+      score: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
+    });
   }
 
   ngOnInit() {
     this.getMovieInformation();
-  }
-
-  // Navigate to update movie page
-  navigateToUpdateMovie() {
-    this.router.navigate(['/movie-edit', this.movie.id]);
   }
 
   private getMovieInformation() {
@@ -40,17 +44,13 @@ export class MovieDetailsComponent implements OnInit {
       return;
     }
 
-    console.log(movieId);
-
     this.movieService.findMovieById(movieId).subscribe({
       next: movie => {
         if (!movie) {
           this.router.navigate(['/error']);
           return;
         }
-        console.log('MOVIE ID ' + movie.id);
         this.movie = movie;
-        // Call getGenresOfMovie after getting movie information
       },
       error: error => {
         console.error('Error fetching movie:', error);
@@ -59,8 +59,38 @@ export class MovieDetailsComponent implements OnInit {
     });
   }
 
+  // Navigate to update movie page
+  navigateToUpdateMovie() {
+    this.router.navigate(['/movie-edit', this.movie.id]);
+  }
+
+  // Navigate to the page for the actor details
   navigateToActorDetails(actorId: number) {
     this.router.navigate(['/actor-details', actorId]);
   }
 
+  // Submit the review
+  submitReview() {
+    console.log("submit review")
+
+    if (this.form.valid) {
+      const reviewRequest: ReviewDto = {
+        userId: Number(localStorage.getItem('userId')),
+        movieId: this.movie.id,
+        text: this.form.value.text!,
+        score: this.form.value.score!
+      };
+
+      console.log(reviewRequest)
+
+      this.reviewService.saveReview(reviewRequest).subscribe({
+        next: review => {
+          window.location.reload();
+        },
+        error: error => {
+          console.error('Error posting review:', error);
+        }
+      });
+    }
+  }
 }
