f17dd60d0aa7ba3892bc4c398fba508b369a5eaa
Alex <alexandru.i.jurju@gmail.com>
2024-05-24 12:06:56 +0300

    fix error on register, use an implicit profile pic when registering

--- Movie-Net-Backend/Movie-Net-Backend/Model/User.cs
+++ Movie-Net-Backend/Movie-Net-Backend/Model/User.cs
@@ -10,7 +10,8 @@ public class User
     [Column("email")] public string Email { get; set; }
     [Column("username")] public string Username { get; set; }
     [Column("password")] public string Password { get; set; }
-    [Column("profile_picture_url")] public string ProfilePictureUrl { get; set; }
+    // todo: find a better way to store the default image
+    [Column("profile_picture_url")] public string ProfilePictureUrl { get; set; } = "https://static-00.iconduck.com/assets.00/profile-default-icon-512x511-v4sw4m29.png"; 
     public virtual ICollection<Movie> Movies { get; set; }
     public virtual ICollection<Review> Reviews { get; set; }
 }
