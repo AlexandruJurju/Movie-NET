0709de78857d94d5655fef79d48a1a42e3d79130
Alex <alexandru.i.jurju@gmail.com>
2024-05-29 10:43:28 +0300

    movie-save angular-material

--- Movie-Net-Frontend/src/app/pages/movie-save/movie-save.component.ts
+++ Movie-Net-Frontend/src/app/pages/movie-save/movie-save.component.ts
@@ -1,38 +1,67 @@
 import {Component} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {MovieDto, MovieService} from "../../services/swagger";
+import {MatCard} from "@angular/material/card";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-movie-save',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatCard,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatLabel,
+    ReactiveFormsModule,
+    MatError,
+    NgIf
   ],
   templateUrl: './movie-save.component.html',
   styleUrl: './movie-save.component.scss'
 })
 export class MovieSaveComponent {
-
+  saveForm: FormGroup = this.formBuilder.group(
+    {
+      title: new FormControl('', [Validators.required]),
+      headline: new FormControl('', [Validators.required]),
+      overview: new FormControl('', [Validators.required]),
+      releaseDate: new FormControl('', [Validators.required]),
+      posterUrl: new FormControl('', [Validators.required]),
+    }
+  )
 
   constructor(
     private movieService: MovieService,
-    private router: Router) {
+    private router: Router,
+    private formBuilder: FormBuilder,) {
   }
 
-
-  onSubmit(movieForm: NgForm) {
-    const movie: MovieDto = movieForm.value;
-
-    this.movieService.saveMovie(movie).subscribe({
-      next: (response) => {
-        this.router.navigate(["/home"]);
-        console.log('Upload successful', response);
-      },
-      error: (error) => {
-        console.error('Upload failed', error);
+  saveMovie() {
+    if (this.saveForm.valid) {
+      const movie: MovieDto = {
+        id: 0,
+        title: this.saveForm.value.title,
+        headline: this.saveForm.value.headline,
+        overview: this.saveForm.value.overview,
+        releaseDate: this.saveForm.value.releaseDate,
+        posterUrl: this.saveForm.value.posterUrl
       }
-    });
 
+      this.movieService.saveMovie(movie).subscribe({
+        next: (response) => {
+          this.router.navigate(["/home"]);
+          console.log('Upload successful', response);
+        },
+        error: (error) => {
+          console.error('Upload failed', error);
+        }
+      });
+    }
   }
 }
