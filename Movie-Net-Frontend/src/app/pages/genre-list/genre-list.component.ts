3dd53fd42ca15f8cc437bc4582daa80274758f4d
Alex <alexandru.i.jurju@gmail.com>
2024-05-29 12:18:07 +0300

    genre-list angular-material

--- Movie-Net-Frontend/src/app/pages/genre-list/genre-list.component.ts
+++ Movie-Net-Frontend/src/app/pages/genre-list/genre-list.component.ts
@@ -2,19 +2,38 @@ import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {Router} from "@angular/router";
 import {GenreDto, GenreService} from "../../services/swagger";
+import {MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable} from "@angular/material/table";
+import {MatButton} from "@angular/material/button";
+import {MatSort} from "@angular/material/sort";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-genre-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatButton,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatSort,
+    MatTooltip,
+    MatPaginator
   ],
   templateUrl: './genre-list.component.html',
   styleUrl: './genre-list.component.scss'
 })
 export class GenreListComponent implements OnInit {
   genres: GenreDto[] = [];
-  genreToDelete: GenreDto = {} as GenreDto;
+  displayedColumns = ['id', 'genreName', 'edit', 'delete']
 
   constructor(private genreService: GenreService, private router: Router) {
   }
@@ -25,24 +44,18 @@ export class GenreListComponent implements OnInit {
 
   findAllGenres(): void {
     this.genreService.findAllGenres().subscribe(genres => this.genres = genres);
+    console.log(this.genres)
   }
 
   // TODO: pass the whole object to page, not just the id, get rid of the call
+
   editGenre(genre: GenreDto): void {
     this.router.navigate(['/genre-edit', genre.id]);
   }
 
   deleteGenre(genreId: number): void {
     this.genreService.deleteGenre(genreId).subscribe(() => {
-      this.findAllGenres(); // Refresh the list after deletion
+      this.findAllGenres();
     });
-  }
-
-  addGenre(): void {
-    this.router.navigate(["/genre-save"])
-  }
-
-  setGenreToDelete(genre: GenreDto) {
-    this.genreToDelete = genre;
   }
 }
